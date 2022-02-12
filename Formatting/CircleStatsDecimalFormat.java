9c560e9d00fc8e6e9a1cf90c5510f2e33a71a6e6
MC Estal <mcestal@u.boisestate.edu>
2022-02-11 19:07:14 -0700

    Formatting Experimentation Completed

--- Formatting/CircleStatsDecimalFormat.java
+++ Formatting/CircleStatsDecimalFormat.java
@@ -26,11 +26,28 @@ public class CircleStatsDecimalFormat
 		circumference = 2 * Math.PI * radius;
 
 		// Round the output to three decimal places
-		DecimalFormat fmt = new DecimalFormat("0.###");
+		DecimalFormat fmt = new DecimalFormat("$00000.000000");
+
+		// I changed it to the result above and it has three more zeroes in front of 12.566371. //
+
+		// I changed this code to a dollar sign and the code came out to have a dollar sign. I guess with this code it just changes the sign of the number or sign of the result. // 
+		// The result earlier was just a regular number, now it changed to a dollar sign. //
 
 		System.out.println("The circle's area: " + fmt.format(area));
 		System.out.println("The circle's circumference: " + fmt.format(circumference));
 
 		scan.close();
+
+		System.out.printf("The circle's area using printf(): %.4f\n", area);
+		System.out.printf("The circle's circumference using printf(): %10.3f\n", circumference);
+		
+
+		// I put 2 pi as the circle's radius and the circle's area is 12.566. //
+		// This came out to be the result because of the math equation that has been set on this code or java. //
+
+	
+
+
+
 	}
 }
