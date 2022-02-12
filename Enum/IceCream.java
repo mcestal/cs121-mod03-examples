de1db4db7dcc8546ef031d685b32629b64f19723
MC Estal <mcestal@u.boisestate.edu>
2022-02-11 18:54:45 -0700

    Enum Activity Completed

--- Enum/IceCream.java
+++ Enum/IceCream.java
@@ -15,7 +15,8 @@ public class IceCream
 		ROCKY_ROAD,
 		MINT_CHOCOLATE_CHIP,
 		COOKIE_DOUGH,
-		HUCKLEBERRY
+		CHOCOLATE_CHIP
+
 	}
 
 	/**
@@ -25,23 +26,36 @@ public class IceCream
 
 		Flavor cone1, cone2, cone3;
 
-		cone1 = Flavor.HUCKLEBERRY;
-		cone2 = Flavor.CHOCOLATE;
+		cone1 = Flavor.CHOCOLATE_CHIP;
+		cone2 = Flavor.VANILLA;
 
 		System.out.println("cone1 value: " + cone1);
 		System.out.println("cone1 ordinal: " + cone1.ordinal());
 		System.out.println("cone1 name: " + cone1.name());
 
-		System.out.println();
-		System.out.println("cone2 value: " + cone2);
-		System.out.println("cone2 ordinal: " + cone2.ordinal());
-		System.out.println("cone2 name: " + cone2.name());
+		System.out.println(); 
+		System.out.println("cone2 value: " + cone2); 
+		System.out.println("cone2 ordinal: " + cone2.ordinal()); 
+		System.out.println("cone2 name: " + cone2.name()); 
 
-		cone3 = cone1;
+		cone3 = cone1; 
 
-		System.out.println();
-		System.out.println("cone3 value: " + cone3);
-		System.out.println("cone3 ordinal: " + cone3.ordinal());
-		System.out.println("cone3 name: " + cone3.name());
+		System.out.println(); 
+		System.out.println("cone3 value: " + cone3); 
+		System.out.println("cone3 ordinal: " + cone3.ordinal()); 
+		System.out.println("cone3 name: " + cone3.name()); 
 	}
+{
+
+boolean result = Flavor.CHOCOLATE_CHIP.ordinal() > Flavor.VANILLA.ordinal();
+System.out.printf("\n\n%s is greater than %s: %b\n",Flavor.CHOCOLATE_CHIP, Flavor.CHOCOLATE, result); 
+
+System.out.printf("%n%n%s is greater than %s: %b%n",Flavor.CHOCOLATE_CHIP, Flavor.CHOCOLATE, result); 
+
+// Nothing chnaged . If you use both codes, the same thing happens. Both results are the same.//
+
+// The first results came out to be chocolate chip and then vanilla and then chocolate chip again. Since my all time favorite is chocolate chip, it came out that chocolate would be the result of all these codes. //
+
+}
+
 }
