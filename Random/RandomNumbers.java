ead8297bf134a5852d211fc6f0aa9bb0e526863d
MC Estal <mcestal@u.boisestate.edu>
2022-02-11 19:20:19 -0700

    Random Experimentation Completed

--- Random/RandomNumbers.java
+++ Random/RandomNumbers.java
@@ -39,5 +39,14 @@ public class RandomNumbers
 		num2 = generator.nextDouble() * 6;  // 0.0 to 5.999999
 		num1 = (int)num2 + 1;
 		System.out.println("From 1 to 6: " + num1);
+
+		generator = new Random(12345);
+
+		// First result: -1555..., 3, 8, 24, 7, 0.3975...., and 3 //
+		// Second result: -5035...., 9, 3, 32, -2, 0.1622...., and 3 //
+		// Third result: 0.1622..., 5, 2, 21, -8, 0.1906...., and 5 // 
+
+		// The result came out to be all different and I belive it is how we the code is set up with each math equations or math codes. //
+
 	}
 }
